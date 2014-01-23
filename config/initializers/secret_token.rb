1fd7e1a6c8e23710a901dc456670315011a6b943
thnkr <patrick@noodle.org>
2014-01-23 13:17:15 -0500

    new dir

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Dashbridge::Application.config.secret_key_base = '2b29a64d6174ac287f1f38103103004b28a30693019e2fd4bb1428bbff02281c2fda1f469d6c88ac58d2a8e5f1eed9da19e9593a3ab017da23b6d3c0a79867e0'
